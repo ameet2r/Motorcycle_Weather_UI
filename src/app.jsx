dea50302d07824164dc14a5156db76e82a2dabb5
Ameet Toor <Ameet_Toor@Hotmail.com>
2025-09-08 20:21:00 -0700

    Initial version that can hit my api

--- src/app.jsx
+++ src/app.jsx
@@ -1,43 +1,22 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
+import { Router } from "preact-router";
+import NewSearchPage from "./routes/NewSearchPage";
+import Forecast from "./routes/Forecast";
+import { Link } from "preact-router/match";
 
-export function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>test src/app.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <header>
+        <h1>Motorcycle Weather</h1>
+        <nav>
+          <Link href="/">New Search</Link>
+          <Link href="/forecast">Previous Searching</Link>
+        </nav>
+      </header>
+      <Router>
+        <NewSearchPage path="/" />
+        <Forecast path="/forecast" />
+      </Router>
+    </div>
+  );
 }
