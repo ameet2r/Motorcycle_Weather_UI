451d909fae6c48cc85f23f2d7f68150c1d42efd1
Ameet Toor <ameet2r@users.noreply.github.com>
2025-09-29 18:15:05 -0700

    Fix React Router future flag warnings

--- src/app.jsx
+++ src/app.jsx
@@ -433,7 +433,7 @@ export default function App() {
     >
       <ThemeProvider>
         <AuthProvider>
-          <Router>
+          <Router future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
             <Routes>
               <Route path="/auth" element={<AuthPage />} />
               <Route path="/*" element={<AppContent />} />
